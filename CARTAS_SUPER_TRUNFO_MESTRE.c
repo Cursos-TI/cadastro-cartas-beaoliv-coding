0b6e90151ad1021393fbde0728c1b9ce25afef4f
beaoliv-coding <beatrizlauriana7738@gmail.com>
2026-04-02 13:13:20 -0300

    Add initial values for master level cards super trunfo

---
+++ CARTAS_SUPER_TRUNFO_MESTRE.c
@@ -0,0 +1,37 @@
+//CRIAÇÃO CARTAS NIVEL MESTRE SUPER TRUNFO
+
+#include <stdio.h>
+
+int main(){
+
+//CRIAÇÃO VARIAVEIS CARTA 1 
+char estado1;
+char codigo1[4];
+char cidade1[50];
+unsigned long int populacao1;
+float area1;
+float pib1;
+int pontos1;
+float densidade1;
+float pibpercapita1;
+float superpoder1;
+
+//CRIAÇÃO VARIAVEIS CARTA 2 
+char estado2;
+char codigo2[4];
+char cidade2[50];
+unsigned long int populacao2;
+float area2;
+float pib2;
+int pontos2;
+float densidade2;
+float pibpercapita2;  
+float superpoder2;
+
+// --- CADASTRO DA CARTA 1 ---
+printf("==============================\n");
+printf("Cadastro da Carta 1           n");
+
+
+  
+}
